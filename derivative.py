53dec2e168b98f11511cac9f13a4d637f611818b
Kerui Wu <99207376+Yujjio@users.noreply.github.com>
2023-02-25 15:28:29 -0500

    test case

--- derivative.py
+++ derivative.py
@@ -7,7 +7,6 @@ def derivative(eq):
     eq_reverse = eq[::-1]
     eq_end = len(eq) - 1 - eq_reverse.find(')')
     equation = eq[eq_start:eq_end]
-    print(equation)
     var = eq[eq_end+3:]
     if var == 'lambda':
         var = 'lamda'
@@ -19,4 +18,4 @@ def derivative(eq):
 
 
 if __name__ == '__main__':
-    print(derivative('d((sin(x) ** 2) * lambda)/dx'))
+    print(derivative('d((sqrt(x)) * lambda)/dx'))
