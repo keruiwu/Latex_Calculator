fd00c5043f90daa8040db1b6b79ae2b38e51f84f
timxie140 <103078266+timxie140@users.noreply.github.com>
2023-02-25 16:41:19 -0500

    Create limit.py

---
+++ limit/limit.py
@@ -0,0 +1,28 @@
+from sympy import *
+
+def cal_limit(or_eq):
+    index_to = or_eq.find("\to")
+    num_to_end = or_eq.find('}')
+    var_to = symbols(or_eq[index_to - 2])
+    num_to = -1
+    if or_eq[index_to + 3].isdigit() or (or_eq[index_to + 3] == '-' and or_eq[index_to + 4].isdigit()):
+        num_to_start = index_to + 3
+        num_to = int(or_eq[num_to_start:num_to_end])
+    elif or_eq[index_to + 4].isalpha():
+        num_to = oo
+    elif (or_eq[index_to + 3] == '-' and or_eq[index_to + 5].isalpha()):
+        num_to = -oo
+
+    limit_str = or_eq[num_to_end+1:]
+    limit_exp = simplify(limit_str) 
+    result = limit(limit_exp, var_to, num_to)
+    if "Limit" in str(result):
+        return "Error -- Not suitable input! Please check your approches value!"
+    return result
+    
+if __name__ == '__main__':
+    test = ["\lim_{x \to \infty} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to -\infty} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 0} (3*x**2+7*x**3)/(x**2+5*x**4)", 
+            "\lim_{x \to 10} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 5} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 3} (7*x**3)/(x**2+5*x**4+x**5+x*8)", 
+            "\lim_{x \to \infty} ((sin (x)) / x)** cos(x)", "\lim_{x \to 0} ((sin (x)) / x)** cos(x)"]
+    for t in test:
+        print(cal_limit(t))
