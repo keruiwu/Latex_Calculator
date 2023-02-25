5b93d8ef960e6f8421c2847fb39243b28164595e
Kerui Wu <99207376+Yujjio@users.noreply.github.com>
2023-02-25 17:59:18 -0500

    integral

--- derivative.py
+++ derivative.py
@@ -5,17 +5,17 @@ def derivative(eq):
     # eq = 'd(sin(lambda) ** 2)/dlambda'
     eq_start = eq.find('(')+1
     eq_reverse = eq[::-1]
-    eq_end = len(eq) - 1 - eq_reverse.find(')')
-    equation = eq[eq_start:eq_end]
-    var = eq[eq_end+3:]
+    eq_end = len(eq) - 1 - eq_reverse.find('/')
+    equation = eq[eq_start:eq_end-1]
+    var = eq[eq_end+2:]
     if var == 'lambda':
         var = 'lamda'
     equation = equation.replace('lambda', 'lamda')
     equation = simplify(equation)
     var = symbols(var)
 
-    return latex(diff(simplify(equation), var))
+    return latex(diff(equation, var))
 
 
 if __name__ == '__main__':
-    print(derivative('d(2*x * lambda)/dlambda'))
+    print(derivative('d(ln(x))/dx'))
