dfd1cf0ffdda37465a3b342c07c90940b166a89b
timxie140 <103078266+timxie140@users.noreply.github.com>
2023-02-26 04:28:50 -0500

    fix limit feature

--- limit.py
+++ limit.py
@@ -16,21 +16,15 @@ def cal_limit(or_eq):
 
     limit_str = or_eq[num_to_end+2:-1]
     limit_exp = simplify(limit_str)
-    print(limit_exp, var_to, num_to)
     result = limit(limit_exp, var_to, num_to)
     if "Limit" in str(result):
         return "Error -- Not suitable input! Please check your approches value!"
-    return result
+    return str(result)
     
 if __name__ == '__main__':
     # test = ["\lim_{x \to \infty} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to -\infty} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 0} (3*x**2+7*x**3)/(x**2+5*x**4)", 
     #         "\lim_{x \to 10} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 5} (3*x**2+7*x**3)/(x**2+5*x**4)", "\lim_{x \to 3} (7*x**3)/(x**2+5*x**4+x**5+x*8)", 
     #         "\lim_{x \to \infty} ((sin (x)) / x)** cos(x)", "\lim_{x \to 0} ((sin (x)) / x)** cos(x)"]
-    test = ["$$\\lim_{x \\to \\infty} \\frac{3x^2+7x^3}{x^2+5x^4}$$", "$$\\lim_{z \\to 0} \\frac{1-cos(z)}{z^2}$$"]
+    test = "\\lim_{x*\\to*\\infty}((3*x**2+7*x**3)/(x**2+5*x**4))"
     
-    for t in test:
-        parse = parsing(t)
-        print(parse)
-        print()
-        print(cal_limit(parse))
-        print()
+    print(cal_limit(test))
