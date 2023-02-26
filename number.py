b393ead5c4bb38b3c9e6f10437f86ef8a680cd51
WUB8 <101521205+WUB8@users.noreply.github.com>
2023-02-25 20:51:17 -0500

    Binary String

---
+++ number.py
@@ -0,0 +1,25 @@
+import sympy
+def binary_convert(I):
+    Num = I
+    remainder = 0
+    div = 0
+    Binary = []
+    Lat_List = []
+    Lat_List.append("Step to convert decimal to Binary String")
+    while(Num!= 1):
+        if Num == I:
+            Lat_List.append("We can do it by divide decimal by 2 until the divisor is 1")
+        div  = Num//2
+        remainder = Num%2
+        Lat_List.append("{} / 2 = {}, The remainder is {}, We write {} to the front of our binary string".format(Num,div,remainder,remainder))  
+        Num = div 
+        Binary.append(remainder)
+    Lat_List.append("Finally, the divisor is 1, 1/2 remains 1, We write 1 to the front of the bianry string and then reverse it")
+    Binary.append(1)
+    Binary.reverse()
+    Lat_List.append("The binary string will be "+"".join(str(item) for item in Binary))
+    return Lat_List
+
+
+print(binary_convert(2))  
+print(bin(8))
